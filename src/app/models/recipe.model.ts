5591eaac2f3f966c4fa17b00284f78b0e1fa47ae
w-5313y <wesleyferreirasb@gmail.com>
2024-02-07 23:47:26 -0300

    Add Ingredient Model

--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,15 +1,7 @@
-export class Recipe{
-  public name: string;
-  public description: string;
-  public imagePath: string;
-
+export class Recipe {
   constructor(
-    name: string,
-    desc: string,
-    imagePath: string
-  ){
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imagePath;
-  }
+    public name: string,
+    public desc: string,
+    public imagePath: string
+  ){}
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
-
+  ingredients = [];
 }
