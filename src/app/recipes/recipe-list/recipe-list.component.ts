73d8eb6ea5b022015a466e7c7e5fc30fd2d7a9d4
w-5313y <wesleyferreirasb@gmail.com>
2024-02-07 23:54:04 -0300

    Adjust Shopping List Style

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Recipe } from '../../models/recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('Tomato', 'a fruit', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgyup5b7eON6YfacLRRcFn0rtmssUh0BPS8vAz9aSAgw&s'),
-    new Recipe('Tomato', 'a fruit', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgyup5b7eON6YfacLRRcFn0rtmssUh0BPS8vAz9aSAgw&s')
+    new Recipe('Apple Pie', 'a pie', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5dnlterecB_QhOrOr5ToVKBfLZInXeXGgMCJXi-vV7A&s'),
+    new Recipe('Tomato Extract', 'sauce', 'https://www.newdirections.com.au/WebRoot/Store/Shops/newdirections/5B51/3185/F4F3/FCDF/85A8/C0A8/DA57/FAA5/EXTR5KTOMATO-5-kg-Tomato-Extract-Fruit-Herbal-Powder-Extracts-L-20190919_ml.jpg')
   ];
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('apple', 6),
+    new Ingredient('tomato', 10)
+  ];
 }
