ac9012c69cc024ce0f34f098b65f60e6a822b4fb
w-5313y <wesleyferreirasb@gmail.com>
2024-02-09 23:23:38 -0300

    [Functionality] Add Ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-edit.component.css'
 })
 export class ShoppingEditComponent {
+  @ViewChild('name') name: ElementRef;
+  @ViewChild('amount') amount: ElementRef;
+  @Output('addIngredient') emitter = new EventEmitter<Ingredient>();
+
+  onAdd(){
+    const item = new Ingredient(this.name.nativeElement.value, this.amount.nativeElement.value);
+    this.emitter.emit(item);
+  }
 
 }
